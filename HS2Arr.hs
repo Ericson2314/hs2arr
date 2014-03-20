a6e5f90a763f1989b7dd11c5757aaa5694b2b647
John Ericson <Ericson2314@Yahoo.com>
2014-03-19 21:41:02 -0400

    moar progress

--- HS2Arr.hs
+++ HS2Arr.hs
@@ -5,13 +5,15 @@ import Control.Applicative
 import Control.Monad
 import Control.Monad.Writer
 
-import Data.List
+import Data.Map as M
+import Data.List (intercalate)
 import Data.Functor.Identity
 
 import Language.Haskell.Syntax
 import Language.Haskell.Parser
 
-for = flip map
+for :: Functor f => f a -> (a -> b) -> f b
+for = flip fmap
 for2 a b f = zipWith f a b
 
 unIdent :: HsName -> String
@@ -21,10 +23,15 @@ encloseSeperate :: String -> String -> String -> [String] -> String
 encloseSeperate _ _ _ [] = []
 encloseSeperate b e s l  = b ++ intercalate s l ++ e
 
+indent :: Functor m => WriterT String m a -> WriterT String m a
+indent = WriterT
+         . (fmap fmap fmap $ \w -> concat $ (\a -> "  " ++ a ++ "\n") <$> lines w)
+         . runWriterT
 
 
 toPyret :: HsModule -> (String, String)
-toPyret (HsModule _ (Module mname) _ _ prog) = (mname, snd $ runWriter $ mapM decl prog)
+toPyret (HsModule _ (Module mname) _ _ prog) =
+  (mname, snd $ runWriter $ foldM decl M.empty prog)
 
 specialCon :: HsSpecialCon -> String
 specialCon a = case a of
@@ -37,38 +44,72 @@ dq (Qual (Module mod) (HsIdent s)) = mod ++ "." ++ s
 dq (UnQual (HsIdent s)) = s
 dq (Special s) = specialCon s
 
+qtyp :: HsQualType -> String
+qtyp (HsQualType [] t) = typ t
+
 typ :: HsType -> String
 typ (HsTyVar (HsIdent tvar)) = tvar
 typ (HsTyCon tqid)           = dq tqid
+typ (HsTyFun t1 t2)          = typ t1 ++ " -> " ++ typ t2
 typ app = uncur [] app
   where uncur :: [HsType] -> HsType -> String
         uncur args (HsTyApp a b) = uncur (b : args) a
-        uncur args t             = typ t ++ (encloseSeperate "<" ">" ", " $ map (uncur []) args)
+        uncur args t             = typ t ++ (encloseSeperate "<" ">" ", " $ fmap (uncur []) args)
 
-decl :: HsDecl -> Writer String ()
+type TMap = Map String HsType
 
-decl (HsTypeDecl _ (HsIdent name) [] t) =
-  tell $ "# s/" ++ name ++"/" ++ typ t
+decl :: TMap -> HsDecl -> Writer String TMap
+decl mp declerations = case declerations of
+  (HsTypeDecl _ (HsIdent name) [] t) -> do
+    tell $ "# s/" ++ name ++"/" ++ typ t ++ "/\n"
+    return mp
 
-decl (HsDataDecl _ _ (HsIdent name) params cons _) = do
-  tell $ "data " ++ name
-  tell $ encloseSeperate "<" ">" ", " $ map unIdent params
-  tell ":\n"
-  forM_ cons $ \(HsConDecl _ (HsIdent name) params) -> do
-    tell $ "  | " ++ name ++ " "
-    tell $ encloseSeperate "(" ")" ", " $ for2 params ([1..] :: [Int]) $
-      \(HsUnBangedTy t) i -> "_p-" ++ show i ++ " :: " ++ typ t
-    tell "\n"
-  tell "end\n\n"
+  (HsDataDecl _ _ (HsIdent name) params cons _) -> do
+    tell $ "data " ++ name
+    tell $ encloseSeperate "<" ">" ", " $ fmap unIdent params
+    tell ":\n"
+    forM_ cons $ \(HsConDecl _ (HsIdent name) params) -> do
+      tell $ "  | " ++ name ++ " "
+      tell $ encloseSeperate "(" ")\n" ", " $ for2 params [1..] $
+        \(HsUnBangedTy t) i -> "_p-" ++ show i ++ " :: " ++ typ t
+    tell "end\n\n"
+    return mp
 
---decl (HsInfixDecl _ HsAssoc Int HsOP) =
+  (HsInfixDecl _ _ _ _) -> error "infix"
+  (HsDefaultDecl _ types) -> error "default"
+  (HsTypeSig _ [HsIdent name] (HsQualType [] t)) -> return $ insert name t mp
 
-ast :: IO (ParseResult HsModule)
+  (HsFunBind topcases) -> do
+    forM_ topcases $ \(HsMatch _ (HsIdent name) pats expr wheres) -> do
+      case pats of
+        [] -> tell $ name ++ " = block:"
+        vars -> do
+          tell $ "fun " ++ name ++ " "
+          tell $ encloseSeperate "(" "):\n" ", " $ for2 vars (splitArgs $ mp ! name) $
+            \(HsPVar (HsIdent var)) t -> var ++ " :: " ++ typ t
+      indent $ block mp expr wheres
+      tell "end\n\n"
+    return mp
+    where splitArgs :: HsType -> [HsType]
+          splitArgs (HsTyFun t1 t2) = t1 : splitArgs t2
+          splitArgs t               = [t]
+
+  (HsPatBind _ pat expr wheres) -> do
+    tell "PATBIND = block:\n"
+    indent $ block mp expr wheres
+    tell "end"
+    return mp
+
+block :: TMap -> t -> [HsDecl] -> Writer String ()
+block mp expr bindings = do
+  foldM_ decl mp bindings
+  tell "EXPR\n"
+
+test :: ParseResult HsModule -> IO ()
+test (ParseOk mod) = putStrLn $ snd $ toPyret mod
+
 ast = parseModule <$> readFile "/home/jcericso/git/pyrec/haskell/Pyrec/AST.hs"
+ast' = test =<< ast
 
-report :: IO (ParseResult HsModule)
-report = parseModule <$> readFile "/home/jcericso/git/pyrec/haskell/Pyrec/HS2Arr.hs"
-
-dst :: IO ()
-dst = do (ParseOk mod) <- ast
-         putStrLn $ snd $ toPyret mod
+report = parseModule <$> readFile "/home/jcericso/git/pyrec/haskell/Pyrec/Report.hs"
+report' = test =<< report
